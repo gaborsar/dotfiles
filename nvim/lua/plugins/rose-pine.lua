cfa3ce54aa182657a45a351824cfd50fc0f35ea7
Gabor Sar <gabor.sar@gmail.com>
2025-11-08 08:08:21 +0100

    updated colors

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -3,5 +3,8 @@ return {
   name = "rose-pine",
   config = function()
     vim.cmd("colorscheme rose-pine")
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end
 }
